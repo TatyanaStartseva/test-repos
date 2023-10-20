fb5b06d9978dfa0bded6c9037afa21ad151b43e0
Tatyana12325 <113295419+Tatyana12325@users.noreply.github.com>
2023-10-20 21:33:39 +0300

    Update task_2.py

--- Лабораторная 2/task_2.py
+++ Лабораторная 2/task_2.py
@@ -1,16 +1,14 @@
-from multiprocessing.spawn import import_main_path
-
-
-salary = float(input("How much do you earn?"))
-spend =float(input("How much do you spend?"))
-float(spend)
-increase=3
-if(spend>salary):
-    for i in range(9):
-        if(salary>spend): break;
-        spend+=spend*(1+(increase/100))
-        salary+=salary
-
-    money_capital=spend-salary
-    import math
-    print(math.ceil(money_capital))
+salary = 5000.00  # Ежемесячная зарплата
+spend = 6000.00  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03
+increase += 1.00
+money_capital_ = salary - spend
+money_capital = 0.00
+if spend > salary:
+    for i in range(1, months, 1):
+        money_capital += (salary - (spend * (increase ** i)))
+money_capital += money_capital_
+import math
+money_capital = abs(money_capital)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", "{:.2f}".format(money_capital))
