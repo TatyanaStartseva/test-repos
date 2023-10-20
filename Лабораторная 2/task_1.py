bd3b01467b7149525b60abd4c4091c9356f93574
Tatyana12325 <113295419+Tatyana12325@users.noreply.github.com>
2023-10-20 21:32:57 +0300

    Update task_1.py

--- Лабораторная 2/task_1.py
+++ Лабораторная 2/task_1.py
@@ -1,20 +1,20 @@
-money_capital = float(input("How much money do you have?"))
-salary = float(input("How much do you earn?"))
-spend =float(input("How much do you spend?"))
-float(spend)
-increase=5
-budget= salary+money_capital
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 5
+budget = salary+money_capital
 month = 0
-if(spend>salary):
+if spend > salary:
     while True:
-        if (budget>spend):
-            budget-=spend;
-            month+=1;
+        if budget > spend:
+            budget -= spend
+            month += 1
         else:
-            break;
-        if(budget>=0):
+            break
+        if budget >= 0:
             spend = spend*(1+(increase/100))
-            budget+=salary;
+            budget += salary
         else:
-            break;
-    print(month)
+            break
+
+print("Количество месяцев, которое можно протянуть без долгов:", month)
